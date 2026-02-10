8e152226fe0fd43623b8993de41e219f1a4f982d
hema <saiakula79989@gmail.com>
2026-02-10 15:58:55 +0530

    fix

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -3,7 +3,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // import { getAnalytics } from "firebase/analytics";
-admissions
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,7 +16,7 @@ const firebaseConfig = {
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 export const auth = getAuth(app); // Exporting Auth instance
 export const db = getFirestore(app); // Exporting Firestore instance
